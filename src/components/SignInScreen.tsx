17edfa0d8b95c74612b199e610c086c059fa57c5
u1aryz <u1aryz.d@gmail.com>
2020-01-29 19:33:21 +0900

    Add email link type authentication

--- src/components/SignInScreen.tsx
+++ src/components/SignInScreen.tsx
@@ -1,19 +1,23 @@
 import * as React from "react";
 import * as firebase from "firebase/app";
+import * as firebaseui from "firebaseui";
 import "firebase/auth";
 import {StyledFirebaseAuth} from "react-firebaseui";
 
 export default (): React.ReactElement => {
   const uiConfig = {
-    signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
       firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+      }
     ],
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
   };
 
   return (
